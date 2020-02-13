801df356f44e80c32151f06134f0def106120615
Akshat Singhal <akshat.singhal2016@vitstudent.ac.in>
2020-02-14 01:21:19 +0530

    Modified Application Properties

--- src/main/java/com/akshat/controller/JiraController.java
+++ src/main/java/com/akshat/controller/JiraController.java
@@ -1,11 +1,9 @@
 package com.akshat.controller;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/jira")
