7208ca5b65e9ff7bae14d30497b170c7ca8e23e3
Akshat Singhal <akshat.singhal2016@vitstudent.ac.in>
2020-02-25 09:47:15 +0530

    Modified Project and Jira module

--- src/main/java/com/akshat/model/Jira.java
+++ src/main/java/com/akshat/model/Jira.java
@@ -19,11 +19,11 @@ public class Jira {
     private Project project;
 
     private float sprint;
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.MERGE)
     @JoinColumn(name = "assignee")
     private Employee assignee;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.MERGE)
     @JoinColumn(name = "assigned_to")
     private Employee assigned_to;
     private String date_created;
