5a871cafccc4574a106d828d7702bce7965aa27b
Akshat Singhal <akshat.singhal2016@vitstudent.ac.in>
2020-03-14 00:21:25 +0530

    Updated Front End

--- src/main/frontend/src/components/Employee/AllEmployees.js
+++ src/main/frontend/src/components/Employee/AllEmployees.js
@@ -1,76 +1,55 @@
 import React, {Component} from 'react';
 import Axios from 'axios';
-import {MDBDataTable} from 'mdbreact';
-import "mdbreact/dist/css/mdb.css";
-import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
 class AllEmployees extends Component
 {
     state = {
-        employees : []
+        employees : [],
+        columnDefs: [{
+            headerName: "Id", field: "id", sortable : true, filter : true, autoHeight : true
+        }, {
+            headerName: "First Name", field: "firstname", sortable : true, filter : true, autoHeight : true
+        },{
+            headerName: "Last Name", field: "lastname", sortable : true, filter : true, autoHeight : true
+        },{
+            headerName: "Address", field: "address", sortable : true, filter : true, autoHeight : true
+        },{
+            headerName: "Phone", field: "phone", sortable : true, filter : true, autoHeight : true
+        },{
+            headerName: "Email", field: "email", sortable : true, filter : true, autoHeight : true
+        },{
+            headerName: "Date of birth", field: "date_of_birth", sortable : true, filter : true, autoHeight : true
+        },{
+            headerName: "Username", field: "username", sortable : true, filter : true, autoHeight : true
+        },{
+            headerName: "Password", field: "password", sortable : true, filter : true, autoHeight : true
+        }]
     }
     componentDidMount(){
-        const URL = "/employee/viewall";
+        const URL = "https://akkiapp.herokuapp.com/employee/viewall";
         Axios.get(URL).then(response => response.data)
-        .then((data)=>{
-            this.setState({employees : data});
-            this.setState({employees : this.state.employees.filter(emp => emp.firstname!=null)})
-            console.log(this.state.employees);
-        });
+            .then((data)=>{
+                this.setState({employees : data});
+                this.setState({employees : this.state.employees.filter(emp => emp.firstname!=null)})
+                console.log(this.state.employees);
+            });
     }
 
     render(){
-        const data = {
-            columns: [
-              {
-                label: 'ID',
-                field: 'id',
-                sort: 'asc'
-              },
-              {
-                label: 'First Name',
-                field: 'firstname',
-                sort: 'asc'
-              },
-              {
-                label: 'Last Name',
-                field: 'lastname',
-                sort: 'asc'
-              },
-              {
-                label: 'Address',
-                field: 'address',
-                sort: 'asc'
-              },
-              {
-                label: 'Phone',
-                field: 'phone',
-                sort: 'asc'
-              },
-              {
-                label: 'Email',
-                field: 'email',
-                sort: 'asc'
-              },
-              {
-                label: 'Date Of birth',
-                field: 'date_of_birth',
-                sort: 'asc'
-              }
-            ],
-            rows: this.state.employees
-        };
         return(
             <div className="row" style={{backgroundColor:"white",padding : "10px"}}>
-                <div className="col-md-12">
-                    <MDBDataTable
-                        striped
-                        hover
-                        responsive
-                        data= {data}
-                        >
-                    </MDBDataTable>
-                        
+
+                <div style={{width : '100%'}} className="col-md-12 ag-theme-balham">
+                    <AgGridReact
+                        domLayout='autoHeight'
+                        colResizeDefault='true'
+                        columnDefs={this.state.columnDefs}
+                        rowData={this.state.employees}>
+                    </AgGridReact>
                 </div>
             </div>
         )
