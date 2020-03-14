625a5b2df20a1291c0d97e0c53e2242cb56d0326
Akshat Singhal <akshat.singhal2016@vitstudent.ac.in>
2020-03-14 20:38:18 +0530

    Updated Front End

---
+++ src/main/frontend/src/components/Jira/IndividualJiraPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class IndividualJiraPage extends Component
+{
+
+    componentDidMount = () =>{
+
+    }
+    render(){
+        return(
+            <div className="row" style={{backgroundColor:"white",padding : "10px"}}>
+                <div className="col-md-12">
+                    <h1>Hello World !! My name is Akshat Singhal</h1>
+                    <p>My name is Akshat Singhal</p>
+                </div>
+            </div>
+        )
+    }
+}
